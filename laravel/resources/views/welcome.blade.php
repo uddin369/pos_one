0161f04793bf4c1ab957d925f8987a43b7b7b9bd
Asad <uddin369@gmail.com>
2017-02-20 14:40:42 -0800

    layout setup

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -9,83 +9,120 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <link href="{{asset('css/local.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width,initial-scale=1">
+            <meta name="description" content="">
+            <meta name="author" content="">
+            <link rel="icon" href="">
+            <title> trans('emp.site_title')}}</title>
 
-            .full-height {
-                height: 100vh;
-            }
+            <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+            <link href="{{asset('css/local.css')}}" rel="stylesheet" type="text/css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+            @yield('custom_page_header')
 
-            .position-ref {
-                position: relative;
-            }
+        </head>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#myPage">Logo</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#about">ABOUT</a></li>
+                    <li><a href="#services">SERVICES</a></li>
+                    <li><a href="#portfolio">PORTFOLIO</a></li>
+                    <li><a href="#pricing">PRICING</a></li>
+                    <li><a href="#contact">CONTACT</a></li>
+                </ul>
             </div>
         </div>
+    </nav>
+
+    <!--
+    End Fixed Navigation
+    ==================================== -->
+
+    <!--	<div id="wrapper">
+            <div id="page-content-wrapper">
+                <div class="container-fluid">
+        -->
+    @yield('content')
+
+    <!--			</div>
+			</div>
+		</div>
+	-->
+
+    <!-- end Contact Area
+========================================== -->
+    <footer class="container-fluid text-center">
+        <a href="#myPage" title="To Top">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+        </a>
+        <div class="container">
+            <div class="row wow fadeInUp" data-wow-duration="500ms">
+                <div class="col-lg-12">
+
+                    <!-- Footer Social Links -->
+                    <div class="social-icon">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                        </ul>
+                    </div>
+                    <!--/. End Footer Social Links -->
+
+                    <!-- copyright -->
+                    <div class="copyright text-center">
+                        <a href="index.html">
+                            <img src="images/logo-meghna.png" alt="Tech Cardinals" width="160px" height="60px" />
+                        </a>
+                        <br />
+
+                        <p>Design And Developed by <a href="http://www.techcardinals.com"> Tech Cardinals</a>. Copyright &copy; 2017. All Rights Reserved.</p>
+                    </div>
+                    <!-- /copyright -->
+
+                </div> <!-- end col lg 12 -->
+            </div> <!-- end row -->
+        </div> <!-- end container -->
+
+        <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" title="Visit w3schools">www.w3schools.com</a></p>
+    </footer>
+
+
+    <!-- end Footer Area
+    ========================================== -->
+
+
+
+    <script src="{{asset('js/jquery-1.11.0.min.js')}}"></script>
+    <!-- Bootstrap 3.1 -->
+    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('js/local.js')}}"></script>
+    <!-- Slitslider -->
+
     </body>
 </html>
